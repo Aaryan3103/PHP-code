5e6f979463a3532a5c91c6dd306e307b2bd4e736
Aaryan3103 <aaryangoel2000@gmail.com>
2025-02-18 12:04:18 +0530

    Add files via upload

---
+++ (55)Count number of words in string.php
@@ -0,0 +1,17 @@
+WAP in php to count number of words in a string 
+<br>
+<!DOCTYPE html>
+</html>
+</body>
+
+<?php
+echo str_word_count("Hello world!");
+?>
+
+<br>
+"This Program is written by Aaryan Goel 0221BCA163"
+</body>
+</html>
+
+
+(56)(55)Lenght of string using built in function.php
